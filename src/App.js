4470f301a79ab3038d7017cb608eb0ea11d59426
sureshmangs <sureshmangs1@gmail.com>
2019-12-18 16:28:55 +0530

    importing main component in App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Main from './components/Main'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <Main />
+
       </div>
     );
   }
