8c0ea7ef2c1f05cb5a00c8f2da47037febe0a3d2
sureshmangs <sureshmangs1@gmail.com>
2019-12-19 12:17:13 +0530

    Modifying strucure of navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class Nav extends Component {
     render() {
         return (
             <div>
                 <div className="navbar navbar-expand-md bg-dark navbar-dark" >
-                    <a className="navbar-brand" href="/"><img src={require('../images/myLogo.png')} alt="logo" /></a>
+                    <Link className="navbar-brand" to="/"><img src={require('../images/myLogo.png')} alt="logo" /></Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="collapsibleNavbar">
                         <ul className="navbar-nav ml-auto mr-5">
                             <li className="nav-item">
-                                <a className="nav-link" href="/">RESUME</a>
+                                <Link className="nav-link" to="/About">ABOUT</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">ABOUT</a>
+                                <Link className="nav-link" to="/Resume" >RESUME</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">CONTACT</a>
+                                <Link className="nav-link" to="/Projects" >PROJECTS</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/Skills">SKILLS</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/Contact">CONTACT</Link>
                             </li>
                             <li className="nav-item">
                                 <a href="https://github.com/sureshmangs" className="github-corner" aria-label="View source on GitHub">
