58ca9e34858faec80416725e5114d63fb9b7ba2f
sureshmangs <sureshmangs1@gmail.com>
2019-12-21 14:40:41 +0530

    corercting the text animation

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,84 +1,88 @@
-import React, { Component } from 'react';
+import React from 'react';
+const { useRef, useState, useReducer, useMemo, useEffect } = React;
 
+const typeWriterInitialState = {
+    index: 0,
+    deleting: false,
+    length: 0
+};
 
-class Main extends Component {
+const typeWriterReducer = (state, action) => {
+    const { index, deleting, length } = state;
+    const { text, fullText } = action;
 
-    typeWriter() {
-        var TxtRotate = function (el, toRotate, period) {
-            this.toRotate = toRotate;
-            this.el = el;
-            this.loopNum = 0;
-            this.period = parseInt(period, 10) || 2000;
-            this.txt = '';
-            this.tick();
-            this.isDeleting = false;
+    if (deleting && text === '') return {
+        index: index + 1,
+        deleting: false,
+        length: length + 1
+    };
+
+    if (!deleting && text === fullText) return {
+        index: index,
+        deleting: true,
+        length: length - 1
+    };
+
+    return {
+        index,
+        deleting,
+        length: length + (deleting ? -1 : 1)
+    };
+};
+
+function useTypeWriter(props) {
+    const ref = useRef(null);
+    const [period] = useState(props.period || 2000);
+    const [rotate] = useState(props.rotate);
+    const [state, update] = useReducer(
+        typeWriterReducer,
+        typeWriterInitialState
+    );
+    const { index, deleting, length } = state;
+    const fullText = rotate[index % rotate.length];
+    const text = fullText.substring(0, length);
+    const delta = useMemo(() => {
+        if (text === fullText && !deleting) return period;
+        if (text === '' && deleting) return 500;
+        return (200 * Math.random() + 100) * (deleting ? 0.5 : 1);
+    }, [text, fullText, deleting, period]);
+
+    useEffect(() => {
+        ref.current.textContent = text;
+
+        const timeout = setTimeout(update, delta, { text, fullText });
+
+        return () => {
+            clearTimeout(timeout);
         };
+    }, [ref, delta, text, fullText, update]);
 
-        TxtRotate.prototype.tick = function () {
-            var i = this.loopNum % this.toRotate.length;
-            var fullTxt = this.toRotate[i];
-
-            if (this.isDeleting) {
-                this.txt = fullTxt.substring(0, this.txt.length - 1);
-            } else {
-                this.txt = fullTxt.substring(0, this.txt.length + 1);
-            }
-
-            this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-
-            var that = this;
-            var delta = 300 - Math.random() * 100;
-
-            if (this.isDeleting) { delta /= 2; }
-
-            if (!this.isDeleting && this.txt === fullTxt) {
-                delta = this.period;
-                this.isDeleting = true;
-            } else if (this.isDeleting && this.txt === '') {
-                this.isDeleting = false;
-                this.loopNum++;
-                delta = 500;
-            }
-
-            setTimeout(function () {
-                that.tick();
-            }, delta);
-        };
-
-        window.onload = function () {
-            var elements = document.getElementsByClassName('txt-rotate');
-            for (var i = 0; i < elements.length; i++) {
-                var toRotate = elements[i].getAttribute('data-rotate');
-                var period = elements[i].getAttribute('data-period');
-                if (toRotate) {
-                    new TxtRotate(elements[i], JSON.parse(toRotate), period);
-                }
-            }
-            // INJECT CSS
-            var css = document.createElement("style");
-            css.type = "text/css";
-            css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-            document.body.appendChild(css);
-        };
-    }
-
-    componentDidMount() {
-        this.typeWriter();
-    }
-
-    render() {
-        return (
-            <div id="main" >
-                <div className="text-center p-5">
-                    <p id="hi" className="mt-5">HEY! I AM</p>
-                    <h1 id="myName">Suresh Mangs</h1>
-                    <h2 id="typeWrite" className="mt-4 mb-5">I'm a
-                     <span className="txt-rotate" data-period="2000" data-rotate='[ " Developer.", " Coder.", " Programmer."]'></span>
-                    </h2>
-                </div>
-            </div>
-        )
-    }
+    return ref;
 }
 
-export default Main;
+const wrapStyle = { borderRight: '0.08em solid #666', color: "#3e64ff", textDecoration: "underline" };
+
+function TypeWriter(props) {
+    const { period, rotate } = props;
+    const wrapEl = useTypeWriter({ period, rotate });
+
+    return (
+        <span ref={wrapEl} style={wrapStyle} />
+    );
+}
+
+const mainRotate = ['Developer.', 'Programmer.'];
+
+function Tempor() {
+    return (
+        <div id="main" >
+            <div className="text-center p-5">
+                <p id="hi" className="mt-5">HEY! I AM</p>
+                <h1 id="myName">Suresh Mangs</h1>
+                <h2 id="typeWrite">I'm a <TypeWriter period={2000} rotate={mainRotate} /></h2>
+            </div>
+        </div>
+    );
+}
+
+export default Tempor;
